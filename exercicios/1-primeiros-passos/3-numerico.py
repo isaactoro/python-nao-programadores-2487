cd35a323c54242fb566993d52c5281bf9ecce293
isaactoro <isaac.toro.v@gmail.com>
2026-05-03 01:27:50 +0000

    numerico actualizacao

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,16 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type(data_nascimento))
 
 # Realize uma operação entre dados do tipo string e inteiro
-
+print(f'eu estodei {1 +1 } anos na universidade')
 
 # Realize uma operação entre dados do tipo inteiro e float
+enteiro = 23
+decimal = 34.56
+print('Multiplicacao', enteiro * decimal)
+print('Round: ', round(enteiro * decimal, 1))
+
+cadena = 'Amelili e titeeeee'
+print(cadena * 3)
